2a89afc7809cb398df9f595937301c2c63e5c6d5
unknown <ebeck703@gmail.com>
2024-01-04 00:38:36 -0500

    added seeds.sql

---
+++ seeds.sql
@@ -0,0 +1,11 @@
+-- seeds.sql example
+USE company_db;
+
+INSERT INTO department (name)
+VALUES ('Sales'), ('Engineering'), ('Finance'), ('Marketing');
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Sales Lead', 100000.00, 1), ('Software Engineer', 120000.00, 2);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('John', 'Doe', 1, NULL), ('Jane', 'Doe', 2, 1);
