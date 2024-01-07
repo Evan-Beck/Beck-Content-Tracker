41e799dccf02c94452d263ff3b5250ae89978672
unknown <ebeck703@gmail.com>
2024-01-06 23:43:18 -0500

    updated promptuser function and added role, employee functions.

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const connection = mysql.createConnection({
 
 connection.connect(err => {
     if (err) throw err;
-    promptuser();
+    promptUser();
 });
 
 function promptUser() {
@@ -20,11 +20,23 @@ function promptUser() {
         message: 'What would you like to do',
         choices: [
             'Add department',
+            'Add role',
+            'Add employee',
+            'Exit'
         ]
     }).then(answer => {
         switch(answer.action) {
             case 'Add department':
                 addDepartment();
+                break;
+            case 'Add role':
+                addRole ();
+                break;
+            case 'Add employee':
+                addEmployee();
+                break;
+            case 'Exit':
+                console.log("Goodbye!");
                 break;
         }
     });
@@ -41,5 +53,61 @@ function addDepartment() {
             if (err) throw err;
             console.log(`Department added: ${answer.departmentName}`);
         });
+    });
+}
+
+function addRole() {
+    inquirer.prompt([
+        {
+        name: 'title',
+        type: 'input',
+        message: 'What is the title for this role?'
+    },
+    {
+        name: 'salary',
+        type: 'input',
+        message: 'What is the salary for this role?'
+    },
+    {
+        name: 'departmentId',
+        type: 'input',
+        message: 'What is the department ID for this role?'
+    }
+]).then(answer => {
+    const query = `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`;
+connection.query(query, [answer.title, parseFloat(answer.salary), parseInt(answer.departmentId)], (err, res) => {
+   if (err) throw err;
+   console.log(`Role added: ${answer.title} in department ${answer.departmentId}`);
+});
+});
+}
+
+function addEmployee () {
+    inquirer.prompt([
+        {
+            name: 'firstName',
+            type: 'input',
+            message: 'What is your first name?'
+        },
+        {
+            name: 'lastName',
+            type: 'input',
+            message: 'What is your last name?'
+        },
+        {
+        name: 'roleId',
+        type: 'input',
+        message: 'What is your role ID?'
+        },
+        {
+            name: 'managerId',
+            type: 'input',
+            message: 'What is your managers ID?'
+        }
+    ]).then(answer => {
+        const query = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ? ,?)`;
+    connection.query(query, [answer.firstName, answer.lastName, parseInt(answer.roleId), answer.managerId ? parseInt(answer.managerId) : null], (err, res) => {
+        console.log(`Employee added: ${answer.firstName} ${answer.lastName}`);
+    });
     });
 }
